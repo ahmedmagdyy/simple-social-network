69ae6870c3b49ab5239a46890a09db5c2cecae7c
ahmedmagdyy <ahmedmagdy2016@gmail.com>
2019-06-08 11:24:23 +0200

    add login and passport token authorization logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+const key = require("../../config/keys");
+const passport = require('passport');
 // GET api/users/test
 // Test user route
 // Public route
@@ -38,7 +40,8 @@ router.post("/register", (req, res) => {
             throw err;
           }
           newUser.password = hash;
-          newUser.save()
+          newUser
+            .save()
             .then(user => res.json(user))
             .catch(err => console.log(err));
         });
@@ -46,5 +49,51 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+// POST api/users/login
+// user login & send JWT token to user
+// Public route
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //findone will return a user object if it exists
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "User Not Found" });
+    }
+
+    //if email is valid will check the password with the hash in db will return boolen
+    bcrypt.compare(password, user.password).then(isMatched => {
+      if (isMatched) {
+        // log user in and send jwt token
+        // to generate jwt we have to pass payload(informations about user we want to include in payload)
+        // and a secret key included also in jwt
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar,
+        };
+        jwt.sign(payload, key.secretKey, { expiresIn: 3600 }, (err, token) => {
+          if (err) throw err;
+          res.json({
+            success: true,
+            token: "Bearer " + token,
+          });
+        });
+      } else {
+        return res.status(400).json({ password: "Password is Incorrect" });
+      }
+    });
+  });
+});
+
+
+// GET api/users/current
+// return current logged in user info
+// private route
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json(req.user)
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require('passport');
 // api routes
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
-
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 // import mongo uri from config files
-const mongodb = require('./config/keys').mongoURI;
+const mongodb = require("./config/keys").mongoURI;
 
 //connect to online mongodb
 mongoose
-    .connect(mongodb, { useNewUrlParser: true })
-    .then( () => console.log("Mongodb Connnected successfully"))
-    .catch( err => console.log(err));
+  .connect(mongodb, { useNewUrlParser: true })
+  .then(() => console.log("Mongodb Connnected successfully"))
+  .catch(err => console.log(err));
 
 const app = express();
 
-//body parser middlewares
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
-app.use('/api/users', users);
-app.use('/api/posts', posts);
-app.use('/api/profile', profile);
+//body parser middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
 //home route
-app.get('/', (req, res) => (res.send("Hello World !")));
+app.get("/", (req, res) => res.send("Hello World !"));
 
 const PORT = process.env.PORT || 7000;
 
